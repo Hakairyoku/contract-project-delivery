8b335310eec1a32646085a8d2eb72adfd4a02e34
Crollexx <crollexx@gmail.com>
2024-07-11 15:57:19 +0300

    done

--- client/src/page/mainPage/MainPage.jsx
+++ client/src/page/mainPage/MainPage.jsx
@@ -1,9 +1,19 @@
-import React from'react';
+import React, { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import App from "../../app/App";
 
 function MainPage({ title }) {
+ 
   return (
-      <div><h1>MainPage</h1></div>
+    <div>
+    <h1>Хочешь ласковую киску? Заходи</h1>
+      <Link to="/Autorization">Авторизация</Link>
+      <Link to="/Registration">Регистрация</Link>
+      <h2></h2>
+    </div>
   );
 }
+
+
 
 export default MainPage;

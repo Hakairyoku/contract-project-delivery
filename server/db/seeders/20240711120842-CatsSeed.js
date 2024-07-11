03cc605e31478885e746202f2dd7f9a92bcfb89b
beAngelFace <be.angelface@gmail.com>
2024-07-11 15:15:57 +0300

    cat`s array

---
+++ server/db/seeders/20240711120842-CatsSeed.js
@@ -0,0 +1,104 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    
+     await queryInterface.bulkInsert('People', [
+      {
+        "name": "Mystic Whiskers",
+        "class": "intuition",
+        "img": "",
+        "place": "Enchanted Forest",
+        "price": 100
+    },
+    {
+        "name": "Shadow Paws",
+        "class": "dark",
+        "img": "",
+        "place": "Shadow Realm",
+        "price": 150
+    },
+    {
+        "name": "Flame Tail",
+        "class": "fire",
+        "img": "",
+        "place": "Volcanic Cavern",
+        "price": 120
+    },
+    {
+        "name": "Aqua Fur",
+        "class": "water",
+        "img": "",
+        "place": "Mystic Lake",
+        "price": 110
+    },
+    {
+        "name": "Star Gaze",
+        "class": "intuition",
+        "img": "",
+        "place": "Celestial Peak",
+        "price": 200
+    },
+    {
+        "name": "Lunar Claw",
+        "class": "dark",
+        "img": "",
+        "place": "Moonlit Grove",
+        "price": 130
+    },
+    {
+        "name": "Blaze Stripe",
+        "class": "fire",
+        "img": "",
+        "place": "Sunset Valley",
+        "price": 140
+    },
+    {
+        "name": "Ripple Paw",
+        "class": "water",
+        "img": "",
+        "place": "Crystal Lagoon",
+        "price": 115
+    },
+    {
+        "name": "Ember Glow",
+        "class": "fire",
+        "img": "",
+        "place": "Smoldering Peaks",
+        "price": 125
+    },
+    {
+        "name": "Eclipse Eye",
+        "class": "dark",
+        "img": "",
+        "place": "Eclipse Hollow",
+        "price": 160
+    },
+    {
+        "name": "Glimmer Fur",
+        "class": "intuition",
+        "img": "",
+        "place": "Starlit Glade",
+        "price": 135
+    },
+    {
+        "name": "Ocean Breeze",
+        "class": "water",
+        "img": "",
+        "place": "Seafoam Cove",
+        "price": 145
+    }
+     ], {});
+    
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
